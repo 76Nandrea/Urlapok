89ff132860297216249fc4a289dd63b5be6c0096
Andrea Nagy <andrea.nagy761201@gmail.com>
2024-04-25 19:52:47 +0200

    Add project files.

---
+++ BlazorApp/Components/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorApp.Components.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string? Fname { get; set; }
+
+        public string? Lname { get; set; }
+        
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public int Price { get; set; }
+
+    }
+}
